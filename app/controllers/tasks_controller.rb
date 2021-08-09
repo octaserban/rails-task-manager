19f37bade1dc4a7de251e074972741fd9c261438
Octavian Serban <octa_serban@yahoo.com>
2021-08-09 16:54:00 +0400

    rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to @task
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to @task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  #strong params
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get 'tasks', to: 'tasks#index'
+
+# NEW TASK - STEP 1 New
+get 'tasks/new', to: 'tasks#new', as: :new_task
+
+get 'tasks/:id', to: 'tasks#show', as: :task
+
+# DELETE a task
+delete 'tasks/:id', to: 'tasks#destroy'
+
+# NEW TASK - STEP 2 Create
+post 'tasks', to: 'tasks#create'
+
+#UPDATE TASK - STEP 1
+get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+#UPDATE TASK - STEP 2
+patch 'tasks/:id', to: 'tasks#update'
 end
